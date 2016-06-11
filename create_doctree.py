6d635e30a24f74abb063e8940a06142f40f3897d
MacHu-GWU <husanhe@gmail.com>
2016-06-10 23:01:38 -0400

    2016-06-10

---
+++ create_doctree.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import docfly
+
+# Uncomment this if you follow Sanhe's Sphinx Doc Style Guide
+#--- Manually Made Doc ---
+doc = docfly.DocTree("source")
+doc.fly()
